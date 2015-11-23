97d3b1da397800305e8cf1e5a8b743ee0e00642d
thscs210 <c0302006@students.katyisd.org>
2015-11-23 13:21:24 -0600

    New changelist

--- app/src/main/java/com/twistedx/superstore/AppleWatch.java
+++ app/src/main/java/com/twistedx/superstore/AppleWatch.java
@@ -2,7 +2,10 @@ package com.twistedx.superstore;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AppleWatch extends Activity {
 
@@ -12,6 +15,32 @@ public class AppleWatch extends Activity {
         setContentView(R.layout.applewatch);
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_store1, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_cart:
+                Intent intent = new Intent(this,Cart.class);
+                startActivity(intent);
+                return true;
+            case R.id.credit:
+                //Code to run when the Create Order item is clicked
+                Intent intent2 = new Intent(this, credit.class);
+                startActivity(intent2);
+                return true;
+            //Code to run when the Create Order item is clicked return true;
+            case R.id.action_settings:
+                //Code to run when the settings item is clicked return true;
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
--- app/src/main/java/com/twistedx/superstore/Store.java
+++ app/src/main/java/com/twistedx/superstore/Store.java
@@ -137,24 +137,27 @@ public class Store extends Activity implements OnItemClickListener {
         Toast.makeText(getApplicationContext(), itemValue, Toast.LENGTH_SHORT).show();
 
     }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-// Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_store1, menu);
         return super.onCreateOptionsMenu(menu);
     }
-    @Override
+
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.credit:
-//Code to run when the Create Order item is clicked
-                Intent intent = new Intent(this, credit.class);
+            case R.id.action_cart:
+                Intent intent = new Intent(this,Cart.class);
                 startActivity(intent);
                 return true;
-            case R.id.action_settings:
-//Code to run when the settings item is clicked
+            case R.id.credit:
+                //Code to run when the Create Order item is clicked
+                Intent intent2 = new Intent(this, credit.class);
+                startActivity(intent2);
                 return true;
+                //Code to run when the Create Order item is clicked return true;
+             case R.id.action_settings:
+                //Code to run when the settings item is clicked return true;
+                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
